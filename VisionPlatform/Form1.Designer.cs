dbbfeea4ee549c251b5a4c3f4a0a166bb45182e4
LAPTOP-TO2HJS03\Yang <1014225598@qq.com>
2021-08-19 10:46:48 +0800

    画线

--- VisionPlatform/Form1.Designer.cs
+++ VisionPlatform/Form1.Designer.cs
@@ -28,13 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(567, 427);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(743, 507);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Vison";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void PictureBox2_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- VisionPlatform/Form1.cs
+++ VisionPlatform/Form1.cs
@@ -12,9 +12,52 @@ namespace VisionPlatform
 {
     public partial class Form1 : Form
     {
+        private Point startPoint;   //直线起点
+        private Point endPoint;    //直线终点
+        private bool isDown;        //鼠标按下标志
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPoint = new Point(e.X, e.Y);
+                isDown = true;
+            }
+        }
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            //绘制曲线
+            e.Graphics.DrawLine(new Pen(Color.Black, 1), startPoint, endPoint);
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                //绘制曲线
+                //Graphics graphics = CreateGraphics();
+                //graphics.DrawLine(new Pen(Color.Black, 1), startPoint, e.Location);
+
+                //记录终点
+                endPoint = e.Location;
+                Refresh();
+
+            }
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+        }
+
         public Form1()
         {
             InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
